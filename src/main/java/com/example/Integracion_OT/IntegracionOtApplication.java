0bf8d1b95a74e4c0975fb3614373e41ad91dae43
EduGL99 <93292498+EduGL99@users.noreply.github.com>
2022-03-28 10:57:00 -0600

    Add files via upload

---
+++ src/main/java/com/example/Integracion_OT/IntegracionOtApplication.java
@@ -0,0 +1,13 @@
+package com.example.Integracion_OT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionOtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionOtApplication.class, args);
+	}
+
+}

4bc4ebb4871043edd7de73e951c325a10e75ca50
EduGL99 <93292498+EduGL99@users.noreply.github.com>
2022-05-16 13:14:31 -0500

    Update TareaController.java

--- src/main/java/com/example/Integracion_OT/controller/TareaController.java
+++ src/main/java/com/example/Integracion_OT/controller/TareaController.java
@@ -53,16 +53,27 @@ public class TareaController {
         }
 
         String projectId = workTicket.getRelatedEntity().get(0).getId();
-        String jsonProject = consultaWO(projectId);
+        String jsonProject = consultaProject(projectId);
         String origenFolio = "";
         String nivelAfectacion = "";
         String afectacionCliente = "";
+        String element = "";
         try{
             JSONObject jsonObject=new JSONObject(jsonProject);
             origenFolio = jsonObject.getJSONObject("data").getJSONObject("project").getJSONArray("properties").getJSONObject(5).getString("stringValue");
             nivelAfectacion = jsonObject.getJSONObject("data").getJSONObject("project").getJSONArray("properties").getJSONObject(7).getString("stringValue");
             afectacionCliente = jsonObject.getJSONObject("data").getJSONObject("project").getJSONArray("properties").getJSONObject(8).getString("stringValue");
+            element = jsonObject.getJSONObject("data").getJSONObject("project").getJSONArray("properties").getJSONObject(10).getJSONObject("nodeValue").getString("id");
         }catch (Exception e){
+            System.out.println("Exception: " + e.toString());
+        }
+
+        String equipment = equipment(element);
+        String idElementoSoc = "";
+        try {
+            JSONObject jsonObject=new JSONObject(equipment);
+            idElementoSoc = jsonObject.getJSONObject("data").getJSONObject("equipment").getJSONArray("properties").getJSONObject(0).getString("rawValue");
+        } catch (Exception e){
             System.out.println("Exception: " + e.toString());
         }
 
@@ -73,7 +84,7 @@ public class TareaController {
         tarea.setVentana(ventana);
         tarea.setFolioGUIO(workTicket.getRelatedEntity().get(0).getName());
         tarea.setServiciosAfectados(origenFolio+"/"+nivelAfectacion+"/"+afectacionCliente);
-//        tarea.setIdElemento();
+        tarea.setIdElemento(idElementoSoc);
         tarea.setFechaCreada(dtf.format(LocalDateTime.now())); // fecha cuando se ejecuta el evento
         tarea.setIdEstatus(1);
         tarea.setIdOrdenMaestra(maestra.getIdOrdenMaestra()); // valor id de la orden maestra
@@ -120,17 +131,17 @@ public class TareaController {
         return crearT;
     }
 
-    public String consultaWO(String projectId){
+    public String consultaProject(String projectId){
         try {
             OkHttpClient client = getUnsafeOkHttpClient();
             MediaType mediaType = MediaType.parse("application/json");
-            okhttp3.RequestBody body = okhttp3.RequestBody.create(mediaType, "{\"query\":\"query proyectOT($projectId: ID!){\\r\\n  project: node(id: $projectId){\\r\\n    ...datos\\r\\n  }\\r\\n}\\r\\n\\r\\nfragment datos on Project{\\r\\n  id\\r\\n  name\\r\\n  properties{\\r\\n    id\\r\\n    stringValue\\r\\n  }\\r\\n}\",\"variables\":{\"projectId\":\""+projectId+"\"}}");
+            okhttp3.RequestBody body = okhttp3.RequestBody.create(mediaType, "{\"query\":\"query proyectOT($projectId: ID!){\\r\\n  project: node(id: $projectId){\\r\\n    ...datos\\r\\n  }\\r\\n}\\r\\n\\r\\nfragment datos on Project{\\r\\n  id\\r\\n  name\\r\\n  properties{\\r\\n    id\\r\\n    stringValue\\r\\n    nodeValue{\\r\\n      name\\r\\n      id\\r\\n    }\\r\\n  }\\r\\n}\",\"variables\":{\"projectId\":\""+projectId+"\"}}");
             Request request = new Request.Builder()
-                    .url("https://symphony.telefonica.com.mx/graph/query")
+                    .url("https://symphony.telefonicamoviles.com.mx/graph/query")
                     .method("POST", body)
                     .addHeader("Authorization", "Basic am9zZWVkdWFyZG8uZ2FyY2lhbHVuYUBudHRkYXRhLmNvbTo2OTY5Njk2OTY5")
                     .addHeader("Content-Type", "application/json")
-                    .addHeader("Cookie", "connect.sid=s%3Ac6bEcxgtwR0XIbYx1h9ozHfVrV-ISJeT.pox8BVCgNKkmYz1WJF6ly2PdlQPW%2BcsOIgTpUl189P0")
+                    .addHeader("Cookie", "connect.sid=s%3AhK_eB-Dx-vDOkiyqhG3m0QbE2l7jcjk3.hMoKtJI0u8GWNctzhOdKLBS5ECeHlGOq%2FxtWy%2Bn%2BKKU")
                     .build();
             Response response = client.newCall(request).execute();
             return Objects.requireNonNull(response.body().string());
@@ -139,13 +150,34 @@ public class TareaController {
         }
     }
 
+    public String equipment(String elementId){
+        try {
+            OkHttpClient client = getUnsafeOkHttpClient();
+            MediaType mediaType = MediaType.parse("application/json");
+            okhttp3.RequestBody body = okhttp3.RequestBody.create(mediaType, "{\"query\":\"query equipment($elementId: ID!){\\r\\n  equipment: node(id: $elementId){\\r\\n    ...datos\\r\\n  }\\r\\n}\\r\\n\\r\\nfragment datos on Equipment{\\r\\n  id\\r\\n  name\\r\\n  properties{\\r\\n    id\\r\\n    rawValue\\r\\n  }\\r\\n}\",\"variables\":{\"elementId\":\""+elementId+"\"}}");
+            Request request = new Request.Builder()
+                    .url("https://symphony.telefonicamoviles.com.mx/graph/query")
+                    .method("POST", body)
+                    .addHeader("Authorization", "Basic am9zZWVkdWFyZG8uZ2FyY2lhbHVuYUBudHRkYXRhLmNvbTo2OTY5Njk2OTY5")
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("Cookie", "connect.sid=s%3AviNVbb_ZVIYqQJHJq-GCrNKxfaytLsLk.HLUws6aAybOhOngXRC%2BB5UykUwxyRAnmJoNANqUzBDM")
+                    .build();
+            Response response = client.newCall(request).execute();
+            return Objects.requireNonNull(response.body().string());
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+
+
     public String createWorkOrder(String name, String workOrderTypeId, String propertyTypeID, String stringValue, String description, String status, String priority){
         try {
             OkHttpClient client = getUnsafeOkHttpClient();
             MediaType mediaType = MediaType.parse("application/json");
             okhttp3.RequestBody body = okhttp3.RequestBody.create(mediaType, "{\"query\":\"mutation workorder($new: AddWorkOrderInput!){ \\r\\n  addWorkOrder(input : $new){ \\r\\n  \\tid\\r\\n    workOrderType{\\r\\n      id\\r\\n      name\\r\\n    }\\r\\n    description\\r\\n    status\\r\\n    priority\\r\\n  }\\r\\n}\",\"variables\":{\"new\":{\"name\":\""+name+"\",\"workOrderTypeId\":\""+workOrderTypeId+"\",\"properties\":[{\"propertyTypeID\":\""+propertyTypeID+"\",\"stringValue\":\""+stringValue+"\"}],\"description\":\""+description+"\",\"status\":\""+status+"\",\"priority\":\""+priority+"\"}}}");
             Request request = new Request.Builder()
-                    .url("https://symphony.telefonica.com.mx/graph/query")
+                    .url("https://symphony.telefonicamoviles.com.mx/graph/query")
                     .method("POST", body)
                     .addHeader("Authorization", "Basic am9zZWVkdWFyZG8uZ2FyY2lhbHVuYUBudHRkYXRhLmNvbTo2OTY5Njk2OTY5")
                     .addHeader("Content-Type", "application/json")
@@ -164,7 +196,7 @@ public class TareaController {
             MediaType mediaType = MediaType.parse("application/json");
             okhttp3.RequestBody body = okhttp3.RequestBody.create(mediaType, "{\"query\":\"mutation upworkorder($new: EditWorkOrderInput!){ \\r\\n  editWorkOrder(input : $new){ \\r\\n  \\tid\\r\\n    workOrderType{\\r\\n      id\\r\\n      name\\r\\n    }\\r\\n    description\\r\\n    status\\r\\n    priority\\r\\n  }\\r\\n}\",\"variables\":{\"new\":{\"id\":\""+id+"\",\"name\":\""+name+"\",\"properties\":[{\"propertyTypeID\":\""+propertyTypeID+"\",\"stringValue\":\""+stringValue+"\"}],\"description\":\""+description+"\",\"status\":\""+status+"\",\"priority\":\""+priority+"\"}}}");
             Request request = new Request.Builder()
-                    .url("https://symphony.telefonica.com.mx/graph/query")
+                    .url("https://symphony.telefonicamoviles.com.mx/graph/query")
                     .method("POST", body)
                     .addHeader("Authorization", "Basic am9zZWVkdWFyZG8uZ2FyY2lhbHVuYUBudHRkYXRhLmNvbTo2OTY5Njk2OTY5")
                     .addHeader("Content-Type", "application/json")
